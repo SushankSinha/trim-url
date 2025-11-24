a69d9f5fc543353ca8c884a3c271bef0adb73c7a
sushanksinha <sushanksinha.5252@gmail.com>
2025-11-25 00:50:57 +0530

    handle auto redirection for links not found

--- client/src/pages/RedirectPage.jsx
+++ client/src/pages/RedirectPage.jsx
@@ -1,14 +1,42 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { showMessage } from "../components/Message";
 
 export default function RedirectPage() {
   const { code } = useParams();
+  const [status, setStatus] = useState("loading");
 
   useEffect(() => {
-    if (code) {
-      window.location.href = `${import.meta.env.VITE_BASE_URL}/redirect/${code}`;
-    }
+    if (!code) return;
+
+    const redirect = async () => {
+      try {
+        const res = await fetch(
+          `${import.meta.env.VITE_BASE_URL}/redirect/${code}`,
+          { redirect: "manual" }
+        );
+
+        if (res.status === 302) {
+          const location = res.headers.get("Location");
+          window.location.href = location;
+        } else if (res.status === 404) {
+          setStatus("notfound");
+          showMessage({msg: "Link not found", type: "error"});
+        } else {
+          setStatus("error");
+        }
+      } catch (err) {
+        setStatus("error");
+        console.error(err)
+      }
+    };
+
+    redirect();
   }, [code]);
 
-  return <p>Redirecting...</p>;
+  if (status === "loading") return <p>Redirecting...</p>;
+  if (status === "notfound") return <div className="card" style={{color:"red"}}>Link not found.</div>;
+  if (status === "error") return <div className="card" style={{color:"red"}}>Server error.</div>;
+
+  return null;
 }
